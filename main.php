ea073df496fc0afdccadf9ce24bb848f0895abf7
laurarzur <laura.arzur@gmail.com>
2025-02-14 16:50:02 +0100

    reconnaissance de la commande list

--- main.php
+++ main.php
@@ -2,4 +2,7 @@
 while (true) {
     $line = readline("Entrez votre commande : ");
     echo "Vous avez saisi : $line\n";
+    if ($line === "list") {
+        echo "affichage de la liste\n";
+    }
 }
